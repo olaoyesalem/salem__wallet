a4c2f156cfabe5a0ae8386ad060e5135c9eb7160
olaoyesalem <olaoyesalemgreat@gmail.com>
2023-07-13 00:53:52 +0000

    Fixed AccountCreate Bugs

--- src/utils/AccountUtils.js
+++ src/utils/AccountUtils.js
@@ -1,41 +1,21 @@
 const { ethers } = require('ethers');
 
-const { Wallet } = require('ethers');
-
-function generateAccount(seedPhrase = "", index = 0) {
-  let wallet;
-
-  // If the seed phrase is not provided, generate a random mnemonic using a CSPRNG
-  if (seedPhrase === "") {
-    seedPhrase = Wallet.createRandom().mnemonic.phrase;
+export function generateKeys(seedPhrase) {
+  if (!seedPhrase) {
+    // Generate a new random seed phrase
+    seedPhrase = ethers.Wallet.createRandom().mnemonic.phrase;
   }
 
-  // If the seed phrase does not contain spaces, it is likely a mnemonic
-  wallet = seedPhrase.includes(" ")
-    ? Wallet.fromMnemonic(seedPhrase, `m/44'/60'/0'/0/${index}`)
-    : new Wallet(seedPhrase);
+  // Derive a wallet from the seed phrase
+  const wallet = ethers.Wallet.fromPhrase(seedPhrase);
 
-  const { address } = wallet;
-  const account = { address, privateKey: wallet.privateKey, balance: "0" };
+  // Get the private key and address from the wallet
+  const privateKey = wallet.privateKey;
+  const address = wallet.address;
 
-  // If the seed phrase does not include spaces, then it's actually a private key, so return a blank string.
-  return { account, seedPhrase: seedPhrase.includes(" ") ? seedPhrase : "" };
+  return {
+    seedPhrase: seedPhrase,
+    privateKey: privateKey,
+    address: address
+  };
 }
-
-module.exports = { generateAccount };
-
-
-
-// // Example usage
-// const walletInfo1 = generateKeysFromSeedPhrase(); // No seed phrase provided
-// console.log('Seed Phrase:', walletInfo1.seedPhrase);
-// console.log('Private Key:', walletInfo1.privateKey);
-// console.log('Address:', walletInfo1.address);
-
-// console.log('-------------------------');
-
-// const seedPhrase = 'your seed phrase goes here';
-// const walletInfo2 = generateKeysFromSeedPhrase(seedPhrase); // Seed phrase provided
-// console.log('Seed Phrase:', walletInfo2.seedPhrase);
-// console.log('Private Key:', walletInfo2.privateKey);
-// console.log('Address:', walletInfo2.address);
